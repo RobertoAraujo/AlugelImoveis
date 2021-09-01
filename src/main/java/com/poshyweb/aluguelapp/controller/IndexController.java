f6ed4aaa39de7734bfd6b6a57d7e5a2fadba9efb
Roberto Moraes <robertomoraes@lampp-it.com.br>
2021-09-01 17:21:55 -0500

    Correção de mapeamento na indexController

--- src/main/java/com/poshyweb/aluguelapp/controller/IndexController.java
+++ src/main/java/com/poshyweb/aluguelapp/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
-	
+	// tela de login
 	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public String paginaInicial() {
 		return "dist/login";
