e01748e9db753f1760654f7ac8f06bf384452953
Roberto Moraes <robertomoraes@lampp-it.com.br>
2021-09-14 10:14:15 -0500

    Alteraçoes no layout e criação de cadastro de inquilino

---
+++ src/main/java/com/poshyweb/aluguelapp/controller/CadastroController.java
@@ -0,0 +1,14 @@
+package com.poshyweb.aluguelapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CadastroController {
+	
+	@RequestMapping (path = "cadastroInquilino", method =  RequestMethod.GET)
+	public String  chamaCadastro() {
+		return "cadastroInquilino";
+	}
+}
